cbcce11e854fc169369d06029c633d5f6ef88a02
axel-garcia <axel.garcia@king.com>
2023-03-28 21:04:07 +0200

    style: :lipstick: Fix text color in formacions page

--- src/pages/formacions.tsx
+++ src/pages/formacions.tsx
@@ -41,47 +41,45 @@ const Formacions = () => {
           </div>
           <div className="lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
             <div className="lg:pr-4">
-              <div className="max-w-xl text-base leading-7 text-gray-700 lg:max-w-lg">
-                <p className="text-lg">
+              <div className="max-w-xl text-lg leading-7 text-gray-500 lg:max-w-lg">
+                <p>
                   <strong className="font-semibold text-gray-900">Relaxació:</strong> desenvolupar la capacitat de viure
                   el moment present per potenciar la concentració i viure amb plenitud. Posem l’èmfasi en aconseguir-ho
                   gràcies a la consciència del nostre cos, la memòria i hàbits que té per tal de donar-li patrons de
                   calma, de pes, de descàrrega, de respiració, alegria amb el moviment...
                 </p>
-                <p className="text-lg">
-                  Treure tensions, físiques i mentals, alleugerir l&#39;organisme en sí i poder-nos relaxar.
-                </p>
-                <p className="mt-6 text-lg">
+                <p>Treure tensions, físiques i mentals, alleugerir l&#39;organisme en sí i poder-nos relaxar.</p>
+                <p className="mt-6">
                   <strong className="font-semibold text-gray-900">Mindfulness o meditació:</strong> respirar
                   conscientment, transitar la calma, la concentració, reduir l’estrès quotidià, connectar amb el cor, la
                   intuïció i la creativitat.
                 </p>
-                <p className="mt-6 text-lg">
+                <p className="mt-6">
                   Meditar és una oportunitat per a crear quietud dins teu sense reaccionar al flux continu de la teva
                   ment: “No som els nostres pensaments, no som les nostres emocions”. D’una manera agradable aprenem a
                   processar sentiments i pensaments sense reaccionar, podent triar amb tranquil·litat les respostes.
                   Prenent distància...
                 </p>
-                <p className="text-lg">
+                <p>
                   Això t’ajuda a rejovenir i relaxar -te, a gestionar millor l&#39;estrès i establir un millor contacte
                   amb les teves relacions, amb els que t’envolten.
                 </p>
                 <h2 className="mt-16 text-2xl font-bold tracking-tight text-gray-900">A qui va dirigit?</h2>
-                <p className="mt-6 text-lg">
+                <p className="mt-6">
                   Per docents i futurs docents. La docència és un ofici que comporta molta dedicació i un “sobrepès”
                   emocional. Per aquesta raó, el recolzament positiu i una destresa personal de gestió de situacions i
                   conflictes “no reactiva” esdevé una molt bona eina.
                 </p>
-                <p className="mt-6 text-lg">
+                <p className="mt-6">
                   En la mida del possible, demana una atenció que anomenem consciència, que vol dir despertar una mirada
                   observadora dins nostre, envers les diferents situacions. Aquest “pilot despert” o atenció interna que
                   s’obté amb la relaxació, amb la meditació, és el què ens permet agafar certa distància en situacions
                   estressants o conflictives i per tant aconseguir que no ens “esgotin” de manera reiterada.
                 </p>
-                <p className="mt-6 text-lg">
+                <p className="mt-6">
                   També per altres col.lectius professionals que us hi veieu reflectits (sanitat, persones cuidadores,…)
                 </p>
-                <p className="mt-6 text-lg">Format adaptable a les necessitats:</p>
+                <p className="mt-6">Format adaptable a les necessitats:</p>
                 <ul className="list-disc">
                   <li className="ml-5 text-lg">
                     Seminaris de 2, 3 ò 4 hores amb continuïtat i/o independents entre sí.
@@ -130,45 +128,45 @@ const Formacions = () => {
           </div>
           <div className="lg:col-span-3 lg:col-start-2 lg:row-start-2 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-1 lg:gap-x-8 lg:px-8">
             <div className="lg:pr-4">
-              <div className="max-w-xl text-base leading-7 text-gray-700 lg:max-w-lg">
-                <p className="text-lg">
+              <div className="max-w-xl text-lg leading-7 text-gray-500 lg:max-w-lg">
+                <p>
                   El Yoga adaptat és l´aplicació del Yoga a col.lectius amb necessitats específiques, on hi ha una
                   diversitat funcional. Que també a vegades s’anomena discapacitat, ja que aquests termes que estan en
                   constant revisió.
                 </p>
-                <p className="mt-6 text-lg">
+                <p className="mt-6">
                   Destil.lem els beneficis del Yoga per tal de que arribin a les persones amb mobilitat reduïda. De la
                   mateixa manera que es lluita perquè tinguin accés a la resta de serveis i activitats que hi ha a la
                   nostra societat. Hi ha diferents situacions i realitats des d’on enfrontem la vida. Certes diferències
                   es converteixen en impediments quan no es pot disposar d’un accés fàcil a recursos i eines útils per a
                   viure i desenvolupar-se en el dia a dia.
                 </p>
-                <p className="mt-6 text-lg">
+                <p className="mt-6">
                   L’objectiu a assolir amb aquesta pràctica no és el d’arribar a aconseguir postures perfectes. I sí que
                   ho és afavorir la consciència corporal i per tant gaudir de més rang de moviments, equilibrar el seu
                   to muscular, millorar la respiració, ajudar a millorar l’autoestima, aprendre a tolerar les
                   frustracions generades per les limitacions físiques o psíquiques i contribuir també a una millor
                   interacció amb els demés.
                 </p>
-                <p className="mt-6 text-lg">En aquests tallers aprendràs a proporcionar un “millor estar”.</p>
-                <p className="mt-6 text-lg">Es tracta de potenciar i centrar-se en allò beneficiós que dona el Yoga.</p>
-                <p className="mt-6 text-lg">
+                <p className="mt-6">En aquests tallers aprendràs a proporcionar un “millor estar”.</p>
+                <p className="mt-6">Es tracta de potenciar i centrar-se en allò beneficiós que dona el Yoga.</p>
+                <p className="mt-6">
                   A l’activar aquests processos, la qualitat de vida de les persones i la seva autonomia personal,
                   augmenta i poden realitzar activitats, també en l’àmbit laboral.
                 </p>
-                <p className="mt-6 text-lg">
+                <p className="mt-6">
                   En aquest estil de Yoga posem el focus molt especialment en les necessitats de cada participant, fent
                   que les diferents activitats, postures, respiracions, etc, ressonin amb les habilitats i capacitats
                   dels practicants. El què importa realment és la persona amb qui tractem. I la relació d’escolta i
                   acompanyament que puguem establir.
                 </p>
-                <p className="mt-6 text-lg">
+                <p className="mt-6">
                   En les formacions obtindràs doncs eines bàsiques per poder enfocar el treball amb aquests col.lectius
                   amb l’objectiu de què guanyin en autonomia personal. Et podrà ser beneficiós tant si t’estàs formant
                   com a instructora de Yoga com si estàs fent una formació en l’àmbit de la integració social, o
                   l’atenció a persones en situació de dependència.
                 </p>
-                <p className="mt-6 text-lg">Format adaptable a les necessitats:</p>
+                <p className="mt-6">Format adaptable a les necessitats:</p>
                 <ul className="list-disc">
                   <li className="ml-5 text-lg">
                     Seminaris de 2, 3 ò 4 hores amb continuïtat i/o independents entre sí.
